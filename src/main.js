631c536476a70aab2def074a72f70ba3783f00d7
= <=>
2021-02-19 11:01:49 -0800

    Add Partials and Add main endpoints.

--- src/main.js
+++ src/main.js
@@ -9,19 +9,58 @@
 // Imports
 const express = require('express')
 const path = require('path')
+const hbs = require('hbs')
 
 const port = 8080
 const app = express()
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
+// Home Page
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Best Community Services'
     })
+})
+
+// Book Space
+app.get('/space', (req, res) => {
+
+})
+
+// Rent Equipment
+app.get('/equipment', (req, res) => {
+
+})
+
+// Order Catering
+app.get('/catering', (req, res) => {
+
+})
+
+// Order Home Care
+app.get('/homecare', (req, res) => {
+
+})
+
+// Order Shuttle services
+app.get('/shuttle', (req, res) => {
+
+})
+
+// Class actions
+app.get('/class', (req, res) => {
+
+})
+
+// Volunteer Sign Up
+app.get('/volunteer', (req, res) => {
+
 })
 
 app.listen(port, () => {
